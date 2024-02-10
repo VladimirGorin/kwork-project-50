3ce27ce23c329c7a8dc14c08f5ff07f321a01291
VladimirGorin <vladimirgorin410@gmail.com>
2024-02-10 16:06:45 +0300

    fix install error

--- utils/install.py
+++ utils/install.py
@@ -8,7 +8,7 @@ try:
         print("Версия Python ниже 3.11.6. Пожалуйста, установите более новую версию.")
         sys.exit(1)
 
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
 except Exception:
     print("Ошибка при установке зависимостей. Убедитесь, что файл requirements.txt существует и содержит правильные зависимости.")
     sys.exit(1)
